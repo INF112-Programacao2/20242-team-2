60b544293a1022f3e2eb649489b46e328b662f9f
Júlia Gomes Reis <156227342+juliagreis@users.noreply.github.com>
2025-01-02 17:22:16 -0300

    removendo parametro id lote + ifndef
    foi removido o parametro id lote da semente, pois os lotes estao linkados a sementes e nao o contrario. Além disso, foi adicionado IFNDEF

--- classeSemente.hpp
+++ classeSemente.hpp
@@ -1,39 +1,50 @@
+#ifndef CLASSE_SEMENTE_HPP
+#define CLASSE_SEMENTE_HPP
+
+
 #include <iostream>
 #include <string>
+
 class semente{
 
-    //atributos:
+    private:
 
-    int _id_lote; //identificador unico do lote de sementes
-    int _id_tipo; //identificador de tipo de semente
-    std::string _nome_cientifico; //nome cientifico da semente
-    std::string _geneIntroduzido;
-    std::string _metodo_producao; 
-    std::string _data_producao;  //formato DD/MM/AAAA
-    std::string _pais_origem;
-    int _quantidade_disponivel;  //kg de sementes disponiveis no lote
-    float _preco_estimado;
-    std::string _statusDisponibilidade;  //em estoque,vendido ou plantado
+
+    //atributos:
+    int _id_tipo; 
+    bool _produz_frutos;
+    int _tempo_colheita;  //indica de quanto em quanto tempo a planta da frutos(meses)
+    std::string _clima_ideal;
+    std::string _solo_ideal;
+    float _irrigacao_ideal;
+    int _expectativaCrescimento; //tempo estimado p/ maturacao da planta(dias)
+    float _expectativaTaxaDeGerminacao; //%
+    float _expectativaTaxaDeSobrevivencia; //%
+    float _expectativaIncidenciaPragasDoencas; //Porcentagem de plantas afetadas por problemas fitossanitários
+
+    public:
 
     //metodos:
 
- semente();   //construtor (em teoria, todos os atributos serao definidos aqui)
+    semente();   //construtor (em teoria, todos os atributos serao definidos aqui)
+    semente(int id);  //sobrecarga do construtor
     ~semente();  //destrutor
 
     void exibirDetalhes();   // Exibe todas as informações da semente
     
     //de inicio, nao faz sentido ter seteres dos atributos dessa classe
 
-    static int _contador_tipo; //controla a quantidade de tipos
-
     //gets:
+    int get_id_tipo();
     int get_tempo_colheita();
     std::string get_clima_ideal();
     std::string get_solo_ideal();
-    std::string get_expectativaResistenciaPragas();
     int get_expectativaCrescimento();
     float get_expectativaTaxaDeGerminacao();
     float get_expectativaTaxaDeSobrevivencia();
     float get_expectativaIncidenciaPragasDoencas(); 
 
+
 };
+
+#endif
