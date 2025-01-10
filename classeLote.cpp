ad947432368a75a31b95d69709b223e70e6bbd37
Júlia Gomes Reis <156227342+juliagreis@users.noreply.github.com>
2025-01-10 16:17:34 -0300

    funcao registrar lote, funcao consumir semente e mudanca construtor
    Agora o registro do lote nao é mais papel do construtor [pendente o construtor por sobrecarga!], adicao do registro de lote(antiga funcao do construtor), adicao da funcao consumir sementes, que recebe o id do lote e a quantidade consumida[mas pode faciilmente ser alterada conforme convem para a classe negociacao]

--- classeLote.cpp
+++ classeLote.cpp
@@ -4,14 +4,32 @@
 lote::lote(){
 
     _statusDisponibilidade="estoque"; 
+    int _id_lote=NULL;
+    semente* _ptr_semente=nullptr;
+    int _id_sementeAssociada=NULL;
+    std::string _nome_cientifico="";
+    std::string _geneIntroduzido="";
+    std::string _metodo_producao="";
+    std::string _data_producao="";
+    std::string _pais_origem="";
+    int _quantidade_disponivel=NULL;
+    float _preco_estimado=NULL;
+    std::string _statusDisponibilidade="";
+
     //____________________________________________//
+}
+lote::~lote(){}
+
+void lote::registrarLote(){
          
     std::fstream arquivoLotes ("Lotes.txt");
     if(!arquivoLotes)
         std::cerr<<"Erro ao abrir o arquivo Lotes.txt\n";
 
+    int contadorDeLotes;
     //identificar qual o id do proximo lote:
-    arquivoLotes>>_id_lote; _id_lote++;         //le o contador de lotes e calcula o ID
+    arquivoLotes>>_id_lote; _id_lote++;         //le o contador de id e calcula o ID
+    arquivoLotes>>contadorDeLotes; contadorDeLotes++;     //le o contador do arquivo e adiciona
  
     //____________________________________________//
     //todo lote está associado a alguma semente, entao
@@ -42,22 +60,21 @@ lote::lote(){
     std::cout<<"País de origem: ";                   std::getline(std::cin,_pais_origem);
     std::cout<<"Quantidade disponível no lote: ";    std::cin>>_quantidade_disponivel;
     std::cout<<"Preco estimado:  ";                  std::cin>>_preco_estimado;
+    
     arquivoLotes.seekp(0, std::ios::end);
-    arquivoLotes<<_id_sementeAssociada<<"+"<<_id_lote<<"+"<<_statusDisponibilidade<<"+"<<_nome_cientifico<<"+"<<_geneIntroduzido<<"+"<<_metodo_producao<<"+"<<
-        _data_producao<<"+"<<_pais_origem<<"+"<<_quantidade_disponivel<<"+"<<_preco_estimado<<"\n";
+    arquivoLotes<<_id_sementeAssociada<<"+"<<_id_lote<<"+"<<_statusDisponibilidade<<"+"<<_nome_cientifico<<
+    "+"<<_geneIntroduzido<<"+"<<_metodo_producao<<"+"<<_data_producao<<"+"<<_pais_origem<<"+"<<
+    _quantidade_disponivel<<"+"<<_preco_estimado<<"\n";
     
     arquivoLotes.seekg(0,std::ios::beg);        //leva o apontador até a posicao do contador do arquivo
-    arquivoLotes<<_id_lote <<std::endl;                    //atualiza o contador
-    
-    arquivoLotes.close();
+    arquivoLotes<<_id_lote <<" "<<contadorDeLotes<<std::endl;                    //atualiza o contador
 
-    //____________________________________________//
+    arquivoLotes.close();
 }
-lote::~lote(){}
 
 void lote::exibirDetalhesDaSemente(){
     _ptr_semente->exibirDetalhes();
-
+    //em andamento
     
 }
 void lote::exibirDetalhes(){
@@ -72,6 +89,72 @@ void lote::exibirDetalhes(){
     for(int i=0;i<50;i++) std::cout<<"-";
     std::cout<<std::endl;
 }
+
+void lote::consumirSementes(int id_lote,int quantidadeConsumida){
+   
+    std::fstream arquivoLotes ("Lotes.txt");   //o arquivo sera lido e escrito  
+    if(!arquivoLotes)
+        std::cerr<<"Erro ao abrir o arquivo Lotes.txt\n";
+
+    int contador_lotes;  //quantos lotes possui o arquivo
+    int controleDeID;    //usado nessa funcao apenas pra ser reescrito no arquivo
+    arquivoLotes>>controleDeID;
+    arquivoLotes>>contador_lotes;
+    arquivoLotes.ignore();
+    
+    std::string *linha=new std::string[contador_lotes];   //aloca as linhas que nao serao alteradas
+      // Lê todas as linhas exceto a que será modificada
+    for(int i=0;i<contador_lotes;i++){
+        if(i+1 == id_lote){
+            // Lê o lote que será consumido
+            arquivoLotes>>_id_sementeAssociada;                       arquivoLotes.ignore();
+            arquivoLotes>>_id_lote;                                   arquivoLotes.ignore();                  
+            getline(arquivoLotes, _statusDisponibilidade, '+');
+            getline(arquivoLotes, _nome_cientifico, '+');     
+            getline(arquivoLotes, _geneIntroduzido, '+');
+            getline(arquivoLotes, _metodo_producao, '+');                                        
+            getline(arquivoLotes, _data_producao, '+');
+            getline(arquivoLotes, _pais_origem, '+');                                        
+            arquivoLotes>>_quantidade_disponivel;                      arquivoLotes.ignore(); 
+            arquivoLotes>>_preco_estimado;                            arquivoLotes.ignore();
+        } else {
+            std::getline(arquivoLotes, linha[i]);
+        }
+    }             
+    
+    //alterando o saldo de sementes: 
+ 
+    if(_quantidade_disponivel-quantidadeConsumida<0){
+        std::cout<<"Quantidade indisponivel!\n";
+        delete[] linha;
+        return;
+    } 
+    else if(_quantidade_disponivel-quantidadeConsumida==0){
+        _quantidade_disponivel=0;
+        std::cout<<"Quantidade disponível!\n";
+        _statusDisponibilidade="vendido";
+    }
+    else{
+        _quantidade_disponivel-=quantidadeConsumida;
+    }
+    arquivoLotes.close();
+
+    //----------escrevendo o arquivo atualizado------//
+    std::ofstream arquivoLotesON("Lotes.txt");
+    arquivoLotesON<<controleDeID<<" "<<contador_lotes<<std::endl;
+    for(int i=0;i<contador_lotes;i++){
+        if(i+1==id_lote){
+            arquivoLotesON<<_id_sementeAssociada<<"+"<<id_lote<<"+"<<_statusDisponibilidade<<"+"<<_nome_cientifico
+            <<"+"<<_geneIntroduzido<<"+"<<_metodo_producao<<"+"<<_data_producao<<"+"<<_pais_origem<<"+"<<
+            _quantidade_disponivel<<"+"<<_preco_estimado<<std::endl;
+        }
+        else
+            arquivoLotesON<<linha[i]<<std::endl;  //escrevendo linhas nao alteradas
+    }
+    delete[] linha;
+    arquivoLotesON.close();
+}
+
 
 
 
