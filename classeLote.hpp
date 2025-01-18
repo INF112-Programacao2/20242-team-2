2d9c05984f4ccf5502b233f184280bade8e45cba
Júlia Gomes Reis <156227342+juliagreis@users.noreply.github.com>
2025-01-18 20:38:27 -0300

    adicionando sobrecarga do construtor q havia esquecido de atualizar

--- classeLote.hpp
+++ classeLote.hpp
@@ -32,17 +32,17 @@ class lote{
     //metodos:
 
     lote();  //construtor
+    lote(int id_semente); //sobrecarga do construtor
     ~lote(); //destrutor
-    void registrarLote();
+
+    void registrarNovoLote(int id_semente);
     void exibirDetalhes();
     void exibirDetalhesDaSemente();
     void set_statusDisponibilidade();  //ou set_statusDisponibilidade
     void set_preco_estimado();  //ou set_preco
     float verificarCompatibilidade(); //% devemos atribuir pesos
-    void consumirSementes(); //diminuir ou zerar a quantidade disponivel no lote
-    void adicionarSementes(); //aumentar o num de lotes ou num de sementes no lote
+    void consumirSementes(int _id_lote,int quantidadeConsumida); //diminuir ou zerar a quantidade disponivel no lote
     void gerarRelatorioSementes();  //polimorfismo?
-
 };
 
 #endif
