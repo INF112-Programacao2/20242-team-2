076eebe4568634710a60e2196e9548e93d002d25
Júlia Gomes Reis <156227342+juliagreis@users.noreply.github.com>
2025-01-25 20:00:46 -0300

    Update Vendedor.hpp

--- Vendedor.hpp
+++ Vendedor.hpp
@@ -16,6 +16,8 @@ private:
     std::string getCurrentDate();
     bool validarData(std::string& data);
     void salvarNegociacao(Negociacao& neg);
+    void buscar_lotes_dessa_semente(int id_semente_busca);
+
 
 public:
     //metodos para area de plantio
@@ -26,8 +28,7 @@ public:
     
     void registrarPlantio();                               //registra o plantio de um lote de sementes na área.
     bool verificarDisponibilidade(int id_area);            //verifica se a área está disponível para plantio.
-    //void compatibilidade_semente();
-    //void buscar_lotes_dessa_semente(int id_semente_busca);                 
+    void compatibilidade_semente();                
 
     //metodos para negociacao
     void registrarNegociacao();                             //em andamento
