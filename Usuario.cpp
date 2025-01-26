f07ef28dc38bf38d3d88909b2976d5cc2261c9fb
Júlia Gomes Reis <156227342+juliagreis@users.noreply.github.com>
2025-01-26 18:30:22 -0300

    atualizando funcao de login

--- Usuario.cpp
+++ Usuario.cpp
@@ -3,6 +3,8 @@
 #include <fstream>
 #include "Usuario.hpp"
 #include "Gestor.hpp"
+#include "Vendedor.hpp"
+#include "Analista.hpp"
 
 Usuario:: Usuario(int id, const std::string& nome, const std::string& email, const std::string& tipoUsuario, 
 const std::string& senha): _id(id), _nome(nome), _email(email), _tipoUsuario(tipoUsuario), _senha(senha){}
@@ -20,14 +22,15 @@ std::string Usuario::getTipoUsuario() const{return _tipoUsuario;}
 void Usuario::setNome(const std::string& nome){_nome = nome;}
 void Usuario::setEmail(const std::string& email){_email = email;}
 
-
-void Usuario::realizarLogin() {
+Usuario* Usuario::realizarLogin() {
     std::ifstream fin("usuarios.txt");
     if (!fin.is_open()) {
         std::cerr << "Erro ao abrir o arquivo usuarios.txt" << std::endl;
-        return;
+        return nullptr;
     }
-    
+
+    Usuario *usuario;
+
     std::string emailInput, senhaInput;
     std::cout << "Insira o email: ";
     std::getline(std::cin, emailInput);
@@ -39,42 +42,46 @@ void Usuario::realizarLogin() {
         std::cout << "Login como Gestor padrão realizado com sucesso!" << std::endl;
         acessarInterface(); // Acessa a interface do gestor diretamente
         fin.close();
-        return;
+        usuario=new Gestor();
+        return usuario;
     }
 
-    std::string linha, acharEmail, acharSenha, tipoUsuario;
+    std::string linha, acharEmail, acharSenha, tipoUsuario, nome;
     bool usuarioEncontrado = false;
 
-    while (std::getline(fin, linha)) {
-        std::getline(fin, linha); // Nome (ignorado)
-        std::getline(fin, acharEmail); // Email
-        std::getline(fin, tipoUsuario); // Tipo de usuário
-        std::getline(fin, acharSenha); // Senha
+    while (std::getline(fin, nome) &&       // Lê o nome
+           std::getline(fin, acharEmail) && // Lê o email
+           std::getline(fin, tipoUsuario) && // Lê o tipo de usuário
+           std::getline(fin, acharSenha)) {  // Lê a senha
 
         // Verifica se email e senha coincidem com algum usuário registrado
         if (acharEmail == emailInput && acharSenha == senhaInput) {
             usuarioEncontrado = true;
 
             if (tipoUsuario == "Gestor") {
+                usuario = new Gestor();
                 std::cout << "Login como Gestor realizado com sucesso!" << std::endl;
             }
             else if (tipoUsuario == "Vendedor") {
+                usuario = new Vendedor();
                 std::cout << "Login como Vendedor realizado com sucesso!" << std::endl;
             }
             else if (tipoUsuario == "Analista") {
+                usuario = new Analista();
                 std::cout << "Login como Analista realizado com sucesso!" << std::endl;
             }
-            
-            acessarInterface(); 
             break;
         }
     }
     fin.close();
+    return usuario;
 
     if (!usuarioEncontrado) {
         std::cout << "Email ou senha incorretos!" << std::endl;
     }
 }
+
+  
 Usuario::Usuario() : _id(0), _nome(""), _email(""), _tipoUsuario(""), _senha("") {}
 
 
