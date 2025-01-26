6fc7892304ea38a572f20fa59a85d5477e7a6b6b
Júlia Gomes Reis <156227342+juliagreis@users.noreply.github.com>
2025-01-25 23:58:00 -0300

    adicionando funcoes buscar e comparar

--- Vendedor.hpp
+++ Vendedor.hpp
@@ -15,6 +15,7 @@ private:
     std::string getCurrentDate();
     bool validarData(std::string& data);
     void salvarNegociacao(Negociacao& neg);
+    void buscar_lotes_dessa_semente(int id_semente_busca);
 
 public:
     //metodos para area de plantio
@@ -25,7 +26,7 @@ public:
     
     void registrarPlantio();                               //registra o plantio de um lote de sementes na área.
     bool verificarDisponibilidade(int id_area);            //verifica se a área está disponível para plantio.
-    //void compatibilidade_semente();
+    void compatibilidade_semente();
     //void buscar_lotes_dessa_semente(int id_semente_busca);                 
 
     //metodos para negociacao
